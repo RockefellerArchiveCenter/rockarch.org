b1a4ef5bb5544e63fe390a0b34f3d7ecd23073f1
Hannah <hsistrunk@gmail.com>
2021-03-23 17:24:56 -0400

    implement aria attributes for disclosure buttons

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,12 +2,15 @@
 
   const mobileSmall = window.matchMedia("(max-width: 580px)")
 
+  // Side navigation toggle
   $(function() {
     $('.rac-side-nav .nav-toggle').on('click', function(e) {
       if ($(this).siblings('.side-nav').is(':visible')) {
         $(this).html('Open Page Menu');
+        $(this).attr("aria-expanded", "false");
       } else {
         $(this).html('Close Page Menu');
+        $(this).attr("aria-expanded", "true");
       }
       $(this).siblings('.side-nav').toggle();
       e.stopPropagation();
@@ -23,8 +26,12 @@
     })
   });
 
+  // Main navigation toggle
   document.querySelector('#nav-toggle').addEventListener('click', function() {
     this.classList.toggle('active');
+    $(this).attr("aria-expanded", function (i, attr) {
+      return attr == "true" ? "false" : "true";
+    });
   });
 
   $('#nav-toggle').click(function() {
