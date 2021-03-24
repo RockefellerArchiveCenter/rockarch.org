622f108244f5b70ba815859cf24682fd1d9a2bc4
Hillel Arnold <helrond@hotmail.com>
2021-03-24 16:41:45 -0400

    formatting

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,42 +1,39 @@
-(function($) {
-
-  const mobileSmall = window.matchMedia("(max-width: 580px)")
-
+(function ($) {
+  const mobileSmall = window.matchMedia('(max-width: 580px)')
   // Side navigation toggle
-  $(function() {
-    $('.rac-side-nav .nav-toggle').on('click', function(e) {
+  $(function () {
+    $('.rac-side-nav .nav-toggle').on('click', function (e) {
       if ($(this).siblings('.side-nav').is(':visible')) {
-        $(this).html('Open Page Menu');
-        $(this).attr("aria-expanded", "false");
+        $(this).html('Open Page Menu')
+        $(this).attr('aria-expanded', 'false')
       } else {
-        $(this).html('Close Page Menu');
-        $(this).attr("aria-expanded", "true");
+        $(this).html('Close Page Menu')
+        $(this).attr('aria-expanded', 'true')
       }
-      $(this).siblings('.side-nav').toggle();
-      e.stopPropagation();
-    });
-    $('html').click(function() {
+      $(this).siblings('.side-nav').toggle()
+      e.stopPropagation()
+    })
+    $('html').click(function () {
       if (mobileSmall.matches) {
-        $('.side-nav').hide();
-        $('.side-nav').siblings('.nav-toggle').html('Open Page Menu');
-      }  else  {
+        $('.side-nav').hide()
+        $('.side-nav').siblings('.nav-toggle').html('Open Page Menu')
+      } else {
         // allow keyboard focus
-         $('.side-nav button').attr('tabindex', '0');
+        $('.side-nav button').attr('tabindex', '0')
       }
     })
-  });
+  })
 
   // Main navigation toggle
-  document.querySelector('#nav-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    $(this).attr("aria-expanded", function (i, attr) {
-      return attr == "true" ? "false" : "true";
-    });
-  });
+  document.querySelector('#nav-toggle').addEventListener('click', function () {
+    this.classList.toggle('active')
+    $(this).attr('aria-expanded', function (i, attr) {
+      return attr === 'true' ? 'false' : 'true'
+    })
+  })
 
-  $('#nav-toggle').click(function() {
-    $('nav .list').toggle();
-    $('nav .header-social-icons').toggle();
-  });
-
-})(jQuery);
+  $('#nav-toggle').click(function () {
+    $('nav .list').toggle()
+    $('nav .header-social-icons').toggle()
+  })
+})(jQuery)
