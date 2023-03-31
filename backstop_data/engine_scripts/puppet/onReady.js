ca749b8353445952b1e9e937bafb1e87e7731d2b
Hannah <hsistrunk@gmail.com>
2023-03-31 16:09:30 -0400

    wait for fonts to load and disable CSS transitions

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -2,5 +2,20 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
-  // add more ready handlers here...
+  // Wait for fonts to load before taking a screenshot
+  await page.evaluateHandle('document.fonts.ready');
 };
+
+// Disable CSS transitions which can cause layout shifts to be delayed until after the screenshot has actually
+// been taken: https://github.com/garris/BackstopJS/issues/1449#issuecomment-1367493309
+const fs = require("fs")
+
+module.exports = async (page) => {
+  // Disable transitions
+  await page.addStyleTag({ content: fs.readFileSync("backstop_data/engine_scripts/backstop.css", "utf-8") })
+
+  // Prompt lazy-loaded images to load
+  await page.evaluate(() => document.querySelectorAll("img[loading=lazy]").forEach(img => img.loading = "eager"))
+
+  await page.waitForNetworkIdle({ idleTime: 250 })
+}
