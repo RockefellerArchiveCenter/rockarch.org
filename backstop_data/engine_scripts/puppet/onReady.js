63f73b7929a314d46ccec8243bf888cf5fbfab59
Hannah <hsistrunk@gmail.com>
2023-04-11 12:09:09 -0400

    update attribution

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -5,8 +5,7 @@ module.exports = async (page, scenario, vp) => {
   // Wait for fonts to load
   await page.evaluateHandle('document.fonts.ready');
 
-  // Disable CSS transitions which can cause layout shifts to be delayed until after the screenshot has actually
-  // been taken: https://github.com/garris/BackstopJS/issues/1449#issuecomment-1367493309
+  // Disable CSS transitions to avoid layout shifts being delayed
   const fs = require("fs")
   await page.addStyleTag({ content: fs.readFileSync("backstop_data/engine_scripts/backstop.css", "utf-8") });
 };
